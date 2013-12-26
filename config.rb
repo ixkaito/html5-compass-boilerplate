3697eb81575bf70642f5852cde74b7b2d70ec742
KITE <kaitokoga@KaitoKoga-iMac.local>
2013-12-26 23:39:36 +0900

    v1.0

---
+++ config.rb
@@ -0,0 +1,9 @@
+http_path = "/"
+css_dir = "_assets/css"
+sass_dir = "_assets/_scss"
+images_dir = "_assets/images"
+javascripts_dir = "_assets/js"
+# line_comments = false
+# output_style = :compact
+# output_style = :compressed
+# sass_options = { :debug_info => true }
